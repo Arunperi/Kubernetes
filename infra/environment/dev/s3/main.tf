312d2f13208725cc4d2ecb104208fa51f3095073
arun peri <arunperi@aruns-MacBook-Air.local>
2026-07-31 13:44:40 +0530

    modified_file

--- infra/environment/dev/s3/main.tf
+++ infra/environment/dev/s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tf_state" {
-    bucket              = "${var.environment}-${var.bucket_name}"
+    bucket              = "${var.environment}-${var.bucket_name}-20260731"
     object_lock_enabled = true
 }
 
