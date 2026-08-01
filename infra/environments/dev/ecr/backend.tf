9b0c9f01ce991ce53f39056865b23cc3ebc0c290
arun peri <arunperi@aruns-MacBook-Air.local>
2026-08-01 14:15:42 +0530

    modified file

--- infra/environments/dev/ecr/backend.tf
+++ infra/environments/dev/ecr/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "dev-tfstate-files-32343"
     key    = "ecr/terraform.tfstate"
-    region = var.aws_region
+    region = "us-east-1"
     encrypt = true
 
     # requires Terraform >= 1.10 and AWS provider >= 5.0
