6077a62236c25d4ac3bece24e8a332108a21df1c
Arun, PVSSP <arun.pvssp@principal.com>
2026-08-07 22:22:22 +0530

    corrected

--- infra/environments/dev/ec2/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "dev-tfstate-files-25111997"
-    key          = "ec2/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
